d61215b1bd14d5207a383a3862e11119a346da14
Latika <--replace-all>
2022-01-16 16:03:33 +0530

    setting up code running from 3000 from bin/www

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello Latika!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
