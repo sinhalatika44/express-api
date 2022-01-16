de86bd757af6dcdf2522942953298f56ffd1e0cc
sinhalatika44 <--replace-all>
2022-01-16 16:18:05 +0530

    500 eror response

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+    error: true,
+    code: 500,
+    e: err.stack,
+  });
 });
 
 module.exports = app;
